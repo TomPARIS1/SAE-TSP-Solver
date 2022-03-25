02d41d2fa37862d11424e357116007f7ff1cbc1b
Tom PARIS <tp082218@iut21.u-bourgogne.fr>
2022-03-25 15:24:39 +0100

    Modification + correction class Fourmis

--- SAE/src/SAE/Fourmis.java
+++ SAE/src/SAE/Fourmis.java
@@ -8,6 +8,7 @@ public class Fourmis {
     protected List<Aretes> areteVisite;
     protected List<Villes> villeVisite;
     protected List<Villes> stockVilles;
+    protected List<Integer> nbAleatoire = new ArrayList<>();
     private Villes villeActuelle;
     private int compteur;
 
@@ -17,46 +18,62 @@ public class Fourmis {
         this.stockVilles = aVisite;
         this.compteur = 0;
         this.villeActuelle = villeActuelle;
+
+        for (int i=1; i < aVisite.size(); i++) {
+            this.nbAleatoire.add(i);
+        }
     }
 
     public void choixChemin (List<List<Aretes>> stockAretes) {
 
         if (!this.stockVilles.isEmpty()) {
-            int destination = (int) Math.random() * (stockAretes.size() - 2 - 2);
+            System.out.println(this.stockVilles.size());
 
-            int depart = villeActuelle.id;
+            int n = (int) (Math.random() * (this.nbAleatoire.size()-1));
+            int destination = this.nbAleatoire.get(n);
 
-            if (!this.villeVisite.contains(villeActuelle)) {
-                this.villeVisite.add(villeActuelle);
-            }
+            if (this.nbAleatoire.contains(destination)) {
+                this.nbAleatoire.remove(n);
 
-            Aretes chemin = stockAretes.get(depart).get(destination);
+                int depart = this.villeActuelle.id;
 
-            if (this.stockVilles.size() == 1) {
-                Villes v1 = this.villeVisite.get(villeVisite.size() - 1);
+                if (!this.villeVisite.contains(this.villeActuelle)) {
+                    this.villeVisite.add(this.villeActuelle);
+                }
 
-                this.areteVisite.add(stockAretes.get(v1.id).get(0));
+                n = destination-1;
+                Aretes chemin = stockAretes.get(depart).get(n);
 
-                this.stockVilles.remove(0);
+                if (this.stockVilles.size() == 1) {
+                    Villes v1 = this.villeVisite.get(this.villeVisite.size() - 1);
 
-                System.out.println(this.compteur);
-            }
+                    this.areteVisite.add(stockAretes.get(v1.id).get(0));
 
-            if (!this.villeVisite.contains(chemin.v2)) {
-                if (Math.random() * (100 - 0) < chemin.visibilite) {
-                    this.villeVisite.add(villeActuelle);
-                    this.areteVisite.add(chemin);
+                    this.stockVilles.remove(0);
 
-                    this.stockVilles.remove(villeActuelle);
-                    villeActuelle = chemin.v2;
+                    System.out.println(this.compteur);
+                }
 
-                    this.compteur += chemin.distance;
-                    choixChemin(stockAretes);
-                } else {
+                if (!this.villeVisite.contains(chemin.v2)) {
+                    if (Math.random() * (100) < chemin.visibilite) {
+                        this.villeVisite.add(this.villeActuelle);
+                        this.areteVisite.add(chemin);
+
+                        this.stockVilles.remove(this.villeActuelle);
+                        this.villeActuelle = chemin.v2;
+
+                        this.compteur += chemin.distance;
+                        choixChemin(stockAretes);
+                    } else {
+                        this.nbAleatoire.add(destination);
+                        choixChemin(stockAretes);
+                    }
+                } else if (this.stockVilles.size() != 0) {
+                    this.nbAleatoire.add(destination);
                     choixChemin(stockAretes);
                 }
-            } else if (this.stockVilles.size() != 0) {
-                System.out.println(this.stockVilles.size());
+            }
+            else {
                 choixChemin(stockAretes);
             }
 
